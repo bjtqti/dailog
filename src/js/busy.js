deae96b0317ee582f7ff69f2e1f5624f35eae1a4
bjtqti <278500368@qq.com>
2015-08-10 22:57:55 +0800

    auto fixed busyicon size

--- src/js/busy.js
+++ src/js/busy.js
@@ -1,11 +1,16 @@
 
+/**
+ * 忙碌光标
+ * @param {string} message 提示信息
+ * @return this
+ */
 function Busy(message){
-	message = message || '';
+    this.message = message;
 	this.init(message);
 }
 
-Busy.prototype.init = function(message){
-	this.message = message;
+
+Busy.prototype.init = function(){
 	 
     //创建全屏遮罩
     var mask = document.createElement('div');
@@ -16,9 +21,10 @@ Busy.prototype.init = function(message){
 
 	mask.innerHTML = icon;
     
-    document.body.appendChild(mask);
-  	//缓存节点
+    //缓存节点
     this.busyLayer = mask;
+
+    document.body.appendChild(mask);
 }
 
 /**
@@ -41,6 +47,44 @@ Busy.prototype.busyIcon = function(){
  * 文字提示信息
  */
 Busy.prototype.mesgbox = function(){
-    var msgbox = '<div class="s-busy-text">'+this.message+'</div>';
-    return msgbox;
+    var text = this.message ||'';
+    return '<div class="s-busy-text">'+text+'</div>';
+}
+
+/**
+ * hide the busyicon
+ */
+Busy.prototype.hide = function(){
+    this.busyLayer.style.display = 'none';
+}
+
+/**
+ * show the busyicon
+ */
+Busy.prototype.show = function(){
+    this.busyLayer.style.display = 'block';
+}
+
+/**
+ * auto fixed the busyicon size
+ */
+Busy.prototype.autofixed = function(){
+    var avaliWidth = Math.min(window.innerWidth,window.innerHeight);
+    var wralpWidth = Math.round(avaliWidth *.3);
+    var spanWidth = wralpWidth * 0.1;
+    var spanHeight = spanWidth * 2;
+    var mgtop = wralpWidth * 0.5;
+    var wrap = this.busyLayer.children[0];
+    //调整光标外围阴影大小
+    wrap.style.cssText = 'width:'+wralpWidth+'px;height:'+wralpWidth+'px;margin:-'+mgtop+'px auto auto -'+mgtop+'px';
+    var smgtop = spanHeight * 0.5;
+    var smgleft = spanWidth * 0.5;
+    //调整叶片大小
+    var css = 'width:'+spanWidth+'px;height:'+spanHeight+'px;margin:-'+smgtop+'px auto auto -'+smgleft+'px';
+    for(var i = 0;i<12;i++){
+        wrap.children[i].style.cssText = css;
+    }
+    //调整文字高度
+    this.busyLayer.children[1].style.marginTop = mgtop + 'px';
+    return this;
 }
